4928640e8ec5004cc7e45e14dab9c6d0693c33ac
DavichoDev <13292121+davicho-dev@users.noreply.github.com>
2023-01-15 12:24:31 -0500

    REFACTOR: Project;

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react'
+
+import './globals.css'
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+	return (
+		<html lang='en'>
+			{/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+			<head />
+			<body>{children}</body>
+		</html>
+	)
+}
+
+export default RootLayout
